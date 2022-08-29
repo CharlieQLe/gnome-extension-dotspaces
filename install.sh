aa21b9a4cd7464beb0c0959226919a8926704e0a
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2022-08-28 20:54:14 -0400

    Created a basic install script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+EXTENSION_PATH="$HOME/.local/share/gnome-shell/extensions"
+EXTENSION_NAME="dotspaces@charlieqle"
+
+rm -r "$EXTENSION_PATH/$EXTENSION_NAME" && cp "$EXTENSION_NAME" "$EXTENSION_PATH" && echo "Log out and log back in to load extensioN!"
+
+
