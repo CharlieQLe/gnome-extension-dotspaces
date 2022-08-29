a241a2bfec5151a9dec916ce667a4f345ab8b482
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2022-08-28 20:56:20 -0400

    Fixed install script.

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 EXTENSION_PATH="$HOME/.local/share/gnome-shell/extensions"
 EXTENSION_NAME="dotspaces@charlieqle"
 
-rm -r "$EXTENSION_PATH/$EXTENSION_NAME"
-cp "$EXTENSION_NAME" "$EXTENSION_PATH" && echo "Log out and log back in to load extension!"
+rm -r "$EXTENSION_PATH/$EXTENSION_NAME" 2> /dev/null
+cp -r "$EXTENSION_NAME" "$EXTENSION_PATH" && echo "Log out and log back in to load extension!"
 
